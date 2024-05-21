9476e0f3c630c42a68bfeabb461c0f6595cbe622
Karlito14 <leiroz26@hotmail.com>
2024-05-21 13:37:50 +0200

    disposition prettier

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,33 +2,33 @@
  * @jest-environment jsdom
  */
 
-import { screen, fireEvent } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
-import BillsUI from "../views/BillsUI.js"
-import { localStorageMock } from "../__mocks__/localStorage.js"
-import mockStore from "../__mocks__/store"
-import { ROUTES, ROUTES_PATH } from "../constants/routes.js"
-import router from "../app/Router.js"
+import { screen, fireEvent } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js";
+import BillsUI from "../views/BillsUI.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
+import router from "../app/Router.js";
 
-jest.mock("../app/Store", () => mockStore) 
+jest.mock("../app/Store", () => mockStore);
 
 beforeEach(() => {
-	document.body.innerHTML = NewBillUI();
-})
+  document.body.innerHTML = NewBillUI();
+});
 
 const onNavigate = (pathname) => {
-  document.body.innerHTML = ROUTES({ pathname })
-}
+  document.body.innerHTML = ROUTES({ pathname });
+};
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
     // test affichage page nouvelle note de frais
     test("Then the newBill should be render", () => {
       //to-do write assertion
-      expect(screen.getAllByText("Envoyer une note de frais")).toBeTruthy()
-    })
-  })
+      expect(screen.getAllByText("Envoyer une note de frais")).toBeTruthy();
+    });
+  });
 
   // test affichage message d'erreur lorsque l'utilisateur choisi un fichier avec la mauvaise extension
   describe("When I upload a file with invalid format", () => {
@@ -39,25 +39,25 @@ describe("Given I am connected as an employee", () => {
         onNavigate,
         store: mockStore,
         localStorage: window.localStorage,
-      })
+      });
 
       // Simulation chargement fichier
-      const handleChangeFile = jest.fn(() => newBill.handleChangeFile)
-      const inputFile = screen.getByTestId("file")
+      const handleChangeFile = jest.fn(() => newBill.handleChangeFile);
+      const inputFile = screen.getByTestId("file");
 
-      inputFile.addEventListener("change", handleChangeFile)
+      inputFile.addEventListener("change", handleChangeFile);
 
       fireEvent.change(inputFile, {
         target: {
           files: [new File(["test.txt"], "test.txt", { type: "image/txt" })],
         },
-      })
+      });
 
       // Message erreur
-      const error = screen.getByTestId("errorFile")
-      expect(error).toBeTruthy()
-    })
-  })
+      const error = screen.getByTestId("errorFile");
+      expect(error).toBeTruthy();
+    });
+  });
 
   // test lorque l'utilisateur choisit un fichier au format valide
   describe("When I upload a file with valid format", () => {
@@ -68,115 +68,112 @@ describe("Given I am connected as an employee", () => {
           type: "Employee",
           email: "azerty@email.com",
         })
-      )
-  
-      const newBill = new NewBill({
-				document,
-				onNavigate,
-				store: mockStore,
-				localStorage: window.localStorage,
-			});
+      );
 
-			const handleChangeFile = jest.fn(() => newBill.handleChangeFile)
-      const inputFile = screen.getByTestId("file")
-
-      inputFile.addEventListener("change", handleChangeFile)
-      
-      fireEvent.change(inputFile, {
-        target: {
-          files: [new File(["test"], "test.jpg", { type: "image/jpg" })],
-        },
-      })
-      
-      const errorVisible = newBill.errorFile
-			expect(errorVisible).toBe(false);
-    })
-  })
-
-  // test affichage message erreur lorsque l'utilisateur choisi une mauvaise date
-  describe("when I choose the wrong date", () => {
-    test("Then it should display an error message, day error", () => {
-      
       const newBill = new NewBill({
         document,
         onNavigate,
         store: mockStore,
         localStorage: window.localStorage,
-      })
+      });
 
-      const handleChangeDate = jest.fn(() => newBill.handleChangeDate)
-      const inputDate = screen.getByTestId("datepicker")
+      const handleChangeFile = jest.fn(() => newBill.handleChangeFile);
+      const inputFile = screen.getByTestId("file");
 
-      inputDate.addEventListener("change", handleChangeDate)
+      inputFile.addEventListener("change", handleChangeFile);
 
-      const today = new Date()
-      let day = today.getDate() + 1
-      let month = today.getMonth() + 1
-      const year = today.getFullYear()
+      fireEvent.change(inputFile, {
+        target: {
+          files: [new File(["test"], "test.jpg", { type: "image/jpg" })],
+        },
+      });
 
-      month = month < 10 ? '0' + month : month
-      day = day < 10 ? '0' + day : day 
+      const errorVisible = newBill.errorFile;
+      expect(errorVisible).toBe(false);
+    });
+  });
+
+  // test affichage message erreur lorsque l'utilisateur choisi une mauvaise date
+  describe("when I choose the wrong date", () => {
+    test("Then it should display an error message, day error", () => {
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        store: mockStore,
+        localStorage: window.localStorage,
+      });
+
+      const handleChangeDate = jest.fn(() => newBill.handleChangeDate);
+      const inputDate = screen.getByTestId("datepicker");
+
+      inputDate.addEventListener("change", handleChangeDate);
+
+      const today = new Date();
+      let day = today.getDate() + 1;
+      let month = today.getMonth() + 1;
+      const year = today.getFullYear();
+
+      month = month < 10 ? "0" + month : month;
+      day = day < 10 ? "0" + day : day;
 
       fireEvent.change(inputDate, {
         target: {
           value: `${year}-${month}-${day}`,
         },
-      })
-      
-      const error = screen.getByTestId("errorDate")
-      expect(error).toBeTruthy()
-    })
-  })
+      });
+
+      const error = screen.getByTestId("errorDate");
+      expect(error).toBeTruthy();
+    });
+  });
 
   // test lorsqu'une bonne date est choisie
   describe("when I choose the right date", () => {
     test("Then it errorDate is false", () => {
-      
       const newBill = new NewBill({
         document,
         onNavigate,
         store: mockStore,
         localStorage: window.localStorage,
-      })
+      });
 
-      const handleChangeDate = jest.fn(() => newBill.handleChangeDate)
-      const inputDate = screen.getByTestId("datepicker")
+      const handleChangeDate = jest.fn(() => newBill.handleChangeDate);
+      const inputDate = screen.getByTestId("datepicker");
 
-      inputDate.addEventListener("change", handleChangeDate)
+      inputDate.addEventListener("change", handleChangeDate);
 
       fireEvent.change(inputDate, {
         target: {
           value: `2024-04-18`,
         },
-      })
-      
-      const error = newBill.errorDate
-      expect(error).toBe(false)
-    })
-  })
+      });
+
+      const error = newBill.errorDate;
+      expect(error).toBe(false);
+    });
+  });
 
   // test lorsque le formulaire est correctement rempli
   describe("When I submit the form completed", () => {
     test("Then the bill is created", () => {
-
       Object.defineProperty(window, "localStorage", {
         value: localStorageMock,
-      })
-      
+      });
+
       window.localStorage.setItem(
         "user",
         JSON.stringify({
           type: "Employee",
           email: "azerty@email.com",
         })
-      )
+      );
 
       const newBill = new NewBill({
         document,
         onNavigate,
         store: mockStore,
         localStorage: window.localStorage,
-      })
+      });
 
       const validBill = {
         type: "Restaurants et bars",
@@ -189,92 +186,91 @@ describe("Given I am connected as an employee", () => {
         fileUrl: "../img/0.jpg",
         fileName: "test.jpg",
         status: "pending",
-      }
+      };
 
-      screen.getByTestId("expense-type").value = validBill.type
-      screen.getByTestId("expense-name").value = validBill.name
-      screen.getByTestId("datepicker").value = validBill.date
-      screen.getByTestId("amount").value = validBill.amount
-      screen.getByTestId("vat").value = validBill.vat
-      screen.getByTestId("pct").value = validBill.pct
-      screen.getByTestId("commentary").value = validBill.commentary
+      screen.getByTestId("expense-type").value = validBill.type;
+      screen.getByTestId("expense-name").value = validBill.name;
+      screen.getByTestId("datepicker").value = validBill.date;
+      screen.getByTestId("amount").value = validBill.amount;
+      screen.getByTestId("vat").value = validBill.vat;
+      screen.getByTestId("pct").value = validBill.pct;
+      screen.getByTestId("commentary").value = validBill.commentary;
 
-      newBill.fileName = validBill.fileName
-      newBill.fileUrl = validBill.fileUrl
+      newBill.fileName = validBill.fileName;
+      newBill.fileUrl = validBill.fileUrl;
 
-      newBill.updateBill = jest.fn()
-      const handleSubmit = jest.fn((e) => newBill.handleSubmit(e))
+      newBill.updateBill = jest.fn();
+      const handleSubmit = jest.fn((e) => newBill.handleSubmit(e));
 
-      const form = screen.getByTestId("form-new-bill")
-      form.addEventListener("submit", handleSubmit)
-      fireEvent.submit(form)
+      const form = screen.getByTestId("form-new-bill");
+      form.addEventListener("submit", handleSubmit);
+      fireEvent.submit(form);
 
-      expect(handleSubmit).toHaveBeenCalled()
-      expect(newBill.updateBill).toHaveBeenCalled()
-    })
-  })
+      expect(handleSubmit).toHaveBeenCalled();
+      expect(newBill.updateBill).toHaveBeenCalled();
+    });
+  });
 
   // test d'intégration POST new bill
   describe("Given I am a user connected as Employee", () => {
     beforeEach(() => {
-      jest.spyOn(mockStore, "bills")
+      jest.spyOn(mockStore, "bills");
 
-      localStorage.setItem("user", JSON.stringify({ type: "Employee", email: "a@a" }));
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-    })
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ type: "Employee", email: "a@a" })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+    });
 
     describe("When I navigate to newBill", () => {
       // Nouvelle facture
       test("promise from mock API POST returns object bills with correct values", async () => {
-        window.onNavigate(ROUTES_PATH.NewBill)
-        
-        const bills = await mockStore.bills().create()
-        expect(bills.key).toBe("1234")
-        expect(bills.fileUrl).toBe("https://localhost:3456/images/test.jpg")
-      }) 
+        window.onNavigate(ROUTES_PATH.NewBill);
+
+        const bills = await mockStore.bills().create();
+        expect(bills.key).toBe("1234");
+        expect(bills.fileUrl).toBe("https://localhost:3456/images/test.jpg");
+      });
 
       // Erreur 404
       test("Then, fetches bills from an API and fails with 404 message error", async () => {
-        window.onNavigate(ROUTES_PATH.NewBill)
-    
+        window.onNavigate(ROUTES_PATH.NewBill);
+
         mockStore.bills.mockImplementationOnce(() => {
           return {
             create: () => {
-              return Promise.reject(new Error("Erreur 404"))
+              return Promise.reject(new Error("Erreur 404"));
             },
-          }
-        })
-        
-			  await new Promise(process.nextTick)
-        document.body.innerHTML = BillsUI(
-          { error: "Erreur 404" }
-          )
-        const message = screen.getByText("Erreur 404")
-        expect(message).toBeTruthy()
-      })
+          };
+        });
+
+        await new Promise(process.nextTick);
+        document.body.innerHTML = BillsUI({ error: "Erreur 404" });
+        const message = screen.getByText("Erreur 404");
+        expect(message).toBeTruthy();
+      });
 
       // Erreur 500
       test("Then, fetches messages from an API and fails with 500 message error", async () => {
         mockStore.bills.mockImplementationOnce(() => {
           return {
             create: () => {
-              return Promise.reject(new Error("Erreur 500"))
+              return Promise.reject(new Error("Erreur 500"));
             },
-            list:()=> {
-              return Promise.resolve([])
-            }
-          }
-        })
-			  await new Promise(process.nextTick)
-        document.body.innerHTML = BillsUI(
-          { error: "Erreur 500" }
-          )
-        const message = screen.getByText("Erreur 500")
-        expect(message).toBeTruthy()
-      })
-    })
-  })
-})
+            list: () => {
+              return Promise.resolve([]);
+            },
+          };
+        });
+        await new Promise(process.nextTick);
+        document.body.innerHTML = BillsUI({ error: "Erreur 500" });
+        const message = screen.getByText("Erreur 500");
+        expect(message).toBeTruthy();
+      });
+    });
+  });
+});
--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -1,34 +1,36 @@
-import { ROUTES_PATH } from '../constants/routes.js'
-import Logout from "./Logout.js"
+import { ROUTES_PATH } from "../constants/routes.js";
+import Logout from "./Logout.js";
 
 export default class NewBill {
   constructor({ document, onNavigate, store, localStorage }) {
-    this.document = document
-    this.onNavigate = onNavigate
-    this.store = store
-    const formNewBill = this.document.querySelector(`form[data-testid="form-new-bill"]`)
-    this.errorDate = false
-    this.errorFile = false
-    formNewBill.addEventListener("submit", this.handleSubmit)
-    const file = this.document.querySelector(`input[data-testid="file"]`)
-    file.addEventListener("change", this.handleChangeFile)
-    const date = this.document.querySelector(`input[data-testid="datepicker"]`)
-    date.addEventListener("change", this.handleChangeDate)
-    this.fileUrl = null
-    this.fileName = null
-    this.billId = null
-    this.EXTENSION_FILES = ['png', 'jpeg', 'jpg']
-    new Logout({ document, localStorage, onNavigate })
+    this.document = document;
+    this.onNavigate = onNavigate;
+    this.store = store;
+    const formNewBill = this.document.querySelector(
+      `form[data-testid="form-new-bill"]`
+    );
+    this.errorDate = false;
+    this.errorFile = false;
+    formNewBill.addEventListener("submit", this.handleSubmit);
+    const file = this.document.querySelector(`input[data-testid="file"]`);
+    file.addEventListener("change", this.handleChangeFile);
+    const date = this.document.querySelector(`input[data-testid="datepicker"]`);
+    date.addEventListener("change", this.handleChangeDate);
+    this.fileUrl = null;
+    this.fileName = null;
+    this.billId = null;
+    this.EXTENSION_FILES = ["png", "jpeg", "jpg"];
+    new Logout({ document, localStorage, onNavigate });
   }
 
   storeNewBill = (error, file) => {
-    this.errorFile = false
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
-    
-    if(error) {
+    this.errorFile = false;
+    const formData = new FormData();
+    const email = JSON.parse(localStorage.getItem("user")).email;
+    formData.append("file", file);
+    formData.append("email", email);
+
+    if (error) {
       error.remove();
     }
 
@@ -37,80 +39,85 @@ export default class NewBill {
       .create({
         data: formData,
         headers: {
-          noContentType: true
-        }
+          noContentType: true,
+        },
       })
-      .then(({filePath, key, fileName}) => {
-        this.billId = key
-        this.fileUrl = filePath
-        this.fileName = fileName
-      }).catch(error => console.error(error))
-  }
+      .then(({ filePath, key, fileName }) => {
+        this.billId = key;
+        this.fileUrl = filePath;
+        this.fileName = fileName;
+      })
+      .catch((error) => console.error(error));
+  };
 
-  displayErrorFile = error => {
+  displayErrorFile = (error) => {
     this.errorFile = true;
-    const inputFile = this.document.querySelector('#file')
-    if(!error) {
-      error = this.document.createElement('p')
-      error.textContent = 'Veuillez ajouter une image au format PNG, JPG ou JPEG'
-      error.classList.add('error-file')
-      error.setAttribute('data-testid', 'errorFile')
-      inputFile.after(error)
+    const inputFile = this.document.querySelector("#file");
+    if (!error) {
+      error = this.document.createElement("p");
+      error.textContent = "Veuillez ajouter une image au format PNG, JPG ou JPEG";
+      error.classList.add("error-file");
+      error.setAttribute("data-testid", "errorFile");
+      inputFile.after(error);
     }
-  }
- 
-  handleChangeFile = e => {
-    e.preventDefault()
-    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-    const fileName = file.name
-    const extension = fileName.split('.').at(-1)
-    const error = this.document.querySelector('.error-file')
+  };
+
+  handleChangeFile = (e) => {
+    e.preventDefault();
+    const file = this.document.querySelector(`input[data-testid="file"]`).files[0];
+    const fileName = file.name;
+    const extension = fileName.split(".").at(-1);
+    const error = this.document.querySelector(".error-file");
 
     if (this.EXTENSION_FILES.includes(extension)) {
-      this.storeNewBill(error, file)
+      this.storeNewBill(error, file);
     } else {
-      this.displayErrorFile(error)
+      this.displayErrorFile(error);
     }
-  }
+  };
 
-  handleChangeDate = e => {
-    const inputDate = this.document.querySelector(`input[data-testid="datepicker"]`)
-    const dateSelected = inputDate.value.split('-')
-    const daySelected = +dateSelected[2]
-    const monthSelected = +dateSelected[1]
-    const yearSelected = +dateSelected[0]
-    const today = new Date()
+  handleChangeDate = (e) => {
+    const inputDate = this.document.querySelector(`input[data-testid="datepicker"]`);
+    const dateSelected = inputDate.value.split("-");
+    const daySelected = +dateSelected[2];
+    const monthSelected = +dateSelected[1];
+    const yearSelected = +dateSelected[0];
+    const today = new Date();
     const day = today.getDate();
     const month = today.getMonth() + 1;
     const year = today.getFullYear();
 
-    let errorDate = this.document.querySelector('.error-date')
+    let errorDate = this.document.querySelector(".error-date");
 
-    if(!errorDate) {
-      errorDate = this.document.createElement('p')
-      errorDate.textContent = 'Veuillez sélectionner une date précédent la date d\'ajourd\'hui'
-      errorDate.setAttribute('class', 'error-date')
-      errorDate.setAttribute('data-testid', 'errorDate')
+    if (!errorDate) {
+      errorDate = this.document.createElement("p");
+      errorDate.textContent = "Veuillez sélectionner une date précédent la date d'ajourd'hui";
+      errorDate.setAttribute("class", "error-date");
+      errorDate.setAttribute("data-testid", "errorDate");
     }
-    
+
     if (yearSelected > year) {
-      this.errorDate = true
+      this.errorDate = true;
     } else if (yearSelected === year && monthSelected > month) {
-      this.errorDate = true    
-    } else if (yearSelected === year && monthSelected === month && daySelected > day) {
-      this.errorDate = true
+      this.errorDate = true;
+    } else if (
+      yearSelected === year &&
+      monthSelected === month &&
+      daySelected > day
+    ) {
+      this.errorDate = true;
     } else {
-      this.errorDate = false
+      this.errorDate = false;
     }
 
-    this.errorDate ? inputDate.after(errorDate) : errorDate.remove()
-  }
+    this.errorDate ? inputDate.after(errorDate) : errorDate.remove();
+  };
 
-  handleSubmit = e => {
-    e.preventDefault()
+  handleSubmit = (e) => {
+    e.preventDefault();
 
-    if(!this.errorDate && !this.errorFile) {
-      const email = JSON.parse(localStorage.getItem("user")).email
+    if (!this.errorDate && !this.errorFile) {
+      const email = JSON.parse(localStorage.getItem("user")).email;
       const bill = {
         email,
         type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
@@ -118,28 +125,30 @@ export default class NewBill {
         amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
         date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
         vat: e.target.querySelector(`input[data-testid="vat"]`).value,
-        pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
+        pct:
+          parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) ||
+          20,
         commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
         fileUrl: this.fileUrl,
         fileName: this.fileName,
-        status: 'pending'
-      }
-      this.updateBill(bill)
-      this.onNavigate(ROUTES_PATH['Bills'])
+        status: "pending",
+      };
+      this.updateBill(bill);
+      this.onNavigate(ROUTES_PATH["Bills"]);
     }
-  }
+  };
 
   // not need to cover this function by tests
   /* istanbul ignore next */
   updateBill = (bill) => {
     if (this.store) {
       this.store
-      .bills()
-      .update({data: JSON.stringify(bill), selector: this.billId})
-      .then(() => {
-        this.onNavigate(ROUTES_PATH['Bills'])
-      })
-      .catch(error => console.error(error))
+        .bills()
+        .update({ data: JSON.stringify(bill), selector: this.billId })
+        .then(() => {
+          this.onNavigate(ROUTES_PATH["Bills"]);
+        })
+        .catch((error) => console.error(error));
     }
-  }
+  };
 }
